1c6c4b861772a97e8571d0f73754d3280e84ccc8
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-03-30 10:25:07 +0200

    Fix codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,15 @@
 require 'timecop'
 require 'simplecov'
 require 'webmock/rspec'
-require 'codeclimate-test-reporter'
 require 'hidemyass'
 require 'pry'
 
 WebMock.disable_net_connect!(allow: 'codeclimate.com')
 WebMock.stub_request(:get, /(incloak|hidester).com/).to_return body: '[]'
 
-CodeClimate::TestReporter.start
-
 SimpleCov.start do
   add_filter '/spec'
   add_filter '/lib/extensions'
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      CodeClimate::TestReporter::Formatter
-    ]
-  )
 end
 
 Dir['lib/**/*.rb'].each { |f| require_relative "../#{f}" }
